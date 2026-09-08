3a3ea33b13d2abf3f0180dea648b9eb9c8419295
Agly <aeligj@gmail.com>
2026-09-08 11:59:28 +0200

    implémentation des bounds maps

--- script/index.js
+++ script/index.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    var map = L.map('map').setView([43.92907, 2.14710], 15);
+    var map = L.map('map', {
+        center: [43.92907, 2.14710],
+        zoom: 14,
+        maxBounds: [
+            [43.94383, 2.11113],
+            [43.91366, 2.17950]
+        ]
+    });
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
