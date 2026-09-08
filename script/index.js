1931af2816b4d7a0f21bfb0de610a17f8ab046e9
Agly <aeligj@gmail.com>
2026-09-08 16:35:20 +0200

    implementation de jawg et des bordure d'albi

--- script/index.js
+++ script/index.js
@@ -1,6 +1,18 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+
+    const BASE_URL = "http://127.0.0.1:5500/";
+    const JSON_URL = BASE_URL + "assets/json/";
+    const STYLE = "jawg-light";
+
+    async function getAlbiJson() {
+        const response = await fetch(JSON_URL + 'albi.json');
+        const json = await response.json();
+
+        return json;
+    }
+
     var map = L.map('map', {
-        center: [43.92907, 2.14710],
+        center: [43.9298, 2.148],
         zoom: 14,
         maxBounds: [
             [43.94383, 2.11113],
@@ -8,8 +20,33 @@ document.addEventListener("DOMContentLoaded", (event) => {
         ]
     });
 
+    /**
+    // Prod 
+    L.tileLayer(`https://tile.jawg.io/${STYLE}/{z}/{x}/{y}{r}.png?access-token=vrwFTDhEI2eLa0OfBBzHnJSNQeGrpQUyLm4zsl2OE5e9XSYHZiWs2ACEnHdV75L1`,
+        {
+            maxZoom: 19,
+            attribution: '&copy; OpenStreetMap contributors &copy; Jawg'
+        }
+    ).addTo(map);
+    **/
+
+    // Test
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+
+    // TODO : boucle sur les bordures
+    
+
+    (async () => {
+        const json = await getAlbiJson();
+
+        console.log(json);
+        L.geoJSON(json).addTo(map);
+    })();
+
+
+    // Jawg contributions :
+    map.attributionControl.addAttribution('<a href="https://www.jawg.io?utm_medium=map&utm_source=attribution" target="_blank">&copy; Jawg</a> - <a href="https://www.openstreetmap.org?utm_medium=map-attribution&utm_source=jawg" target="_blank">&copy; OpenStreetMap</a>&nbsp;contributors')
 });
