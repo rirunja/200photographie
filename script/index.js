e30cc94db5cb014e0e0a8d7a9e5aaba0bc4c0a08
Ethan Besse <ethan_besse@outlook.fr>
2026-09-09 14:03:15 +0200

    ajout de l'INU

--- script/index.js
+++ script/index.js
@@ -18,6 +18,13 @@ document.addEventListener("DOMContentLoaded", (event) => {
         return json.markers;
     }
 
+    async function getBuildingListJson() {
+        const response = await fetch(JSON_URL + 'buildingList.json');
+        const json = await response.json();
+
+        return json;
+    }
+
     var map = L.map('map', {
         center: [43.9298, 2.148],
         zoom: 14,
@@ -60,11 +67,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
+   
+
     // TODO : boucle sur les bordures
     
     (async () => {
         const json = await getAlbiJson();
-
         L.geoJSON(json, {
             style: {
                 "color": "#EB5E28",
